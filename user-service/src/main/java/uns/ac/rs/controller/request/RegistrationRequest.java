588c22a972650da34934fe2acaf87489551b33af
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2024-05-19 18:31:13 +0200

    feat: add regex validation for email and not null validation for all fields

--- user-service/src/main/java/uns/ac/rs/controller/request/RegistrationRequest.java
+++ user-service/src/main/java/uns/ac/rs/controller/request/RegistrationRequest.java
@@ -1,6 +1,7 @@
 package uns.ac.rs.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -12,21 +13,29 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RegistrationRequest {
     @NotBlank(message="Username cannot be empty")
+    @NotNull(message = "Username cannot be null")
     @Size(max = 200, message = "Username cannot have more than 200 characters")
     private String username;
     @NotBlank(message="Password cannot be empty")
+    @NotNull(message = "Password cannot be null")
     private String password;
     @NotBlank(message = "Email cannot be empty")
+    @NotNull(message = "Email cannot be null")
+    @Pattern(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", message = "Invalid email address")
     private String email;
     @NotBlank(message = "First name cannot be empty")
+    @NotNull(message = "First name cannot be null")
     @Size(max = 200, message = "First name cannot have more than 200 characters")
     private String firstName;
     @NotBlank(message = "Last name cannot be empty")
+    @NotNull(message = "Last name cannot be null")
     @Size(max = 200, message = "Last name cannot have more than 200 characters")
     private String lastName;
     @NotBlank(message = "City cannot be empty")
+    @NotNull(message = "City cannot be null")
     @Size(max = 200, message = "City cannot have more than 1000 characters")
     private String city;
     @Pattern(regexp = "GUEST|HOST", message = "Role can be GUEST or HOST")
+    @NotNull(message = "Role cannot be null")
     private String role;
 }
