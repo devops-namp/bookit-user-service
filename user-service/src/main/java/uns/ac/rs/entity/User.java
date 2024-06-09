33c10c03663786c95993eaf1584988748690dc81
NevenaRadesic <102974378+NevenaRadesic@users.noreply.github.com>
2024-06-09 11:22:32 +0200

    refactor: deleted commented lines

--- user-service/src/main/java/uns/ac/rs/entity/User.java
+++ user-service/src/main/java/uns/ac/rs/entity/User.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "bookit_users")
+@Table(name = "bookit-users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- user-service/src/main/resources/data-postgres.sql
+++ user-service/src/main/resources/data-postgres.sql
@@ -1,7 +1,7 @@
 -- PASSWORD: MojaSifra123
 
---INSERT INTO bookit_users (username, password, email, role, firstname, lastname, city)
---VALUES ('username1', 'y4t6TyLTziBF7p9CT75tfqTGmMiNMAs5dyzMZKL2e9g=', 'name@example.com', 'GUEST', 'Ime', 'Prezime', 'Novi Sad');
---
---INSERT INTO bookit_users (username, password, email, role, firstname, lastname, city)
---VALUES ('username2', 'y4t6TyLTziBF7p9CT75tfqTGmMiNMAs5dyzMZKL2e9g=', 'name2@example.com', 'HOST', 'Ime', 'Prezime', 'Novi Sad');
+INSERT INTO bookit-users (username, password, email, role, firstname, lastname, city)
+VALUES ('username1', 'y4t6TyLTziBF7p9CT75tfqTGmMiNMAs5dyzMZKL2e9g=', 'name@example.com', 'GUEST', 'Ime', 'Prezime', 'Novi Sad');
+
+INSERT INTO bookit-users (username, password, email, role, firstname, lastname, city)
+VALUES ('username2', 'y4t6TyLTziBF7p9CT75tfqTGmMiNMAs5dyzMZKL2e9g=', 'name2@example.com', 'HOST', 'Ime', 'Prezime', 'Novi Sad');
