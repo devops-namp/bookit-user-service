e37c80722a99573e7629edab24eb64ea05c56815
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2024-06-19 00:38:27 +0200

    fix: table name in sql files

--- user-service/src/test/resources/data-test.sql
+++ user-service/src/test/resources/data-test.sql
@@ -1,9 +1,9 @@
 -- PASSWORD: MojaSifra123
 
-INSERT INTO bookit-users (username, password, email, role, firstname, lastname, city)
+INSERT INTO public."bookit-users" (username, password, email, role, firstname, lastname, city)
 VALUES ('username1', 'y4t6TyLTziBF7p9CT75tfqTGmMiNMAs5dyzMZKL2e9g=', 'name@example.com', 'GUEST', 'Ime', 'Prezime', 'Novi Sad');
 
-INSERT INTO bookit-users (username, password, email, role, firstname, lastname, city)
+INSERT INTO public."bookit-users" (username, password, email, role, firstname, lastname, city)
 VALUES ('username2', 'y4t6TyLTziBF7p9CT75tfqTGmMiNMAs5dyzMZKL2e9g=', 'name2@example.com', 'HOST', 'Ime', 'Prezime', 'Novi Sad');
 
 INSERT INTO registration_info (city, code, email, firstname, lastname, password, role, username)
