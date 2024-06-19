f17fb87d9553137d26a57bde8914ed3a5e17b7ae
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2024-06-19 20:16:21 +0200

    chore: add more data to sql file

--- user-service/src/main/resources/data-postgres.sql
+++ user-service/src/main/resources/data-postgres.sql
@@ -5,3 +5,6 @@ VALUES ('username1', 'y4t6TyLTziBF7p9CT75tfqTGmMiNMAs5dyzMZKL2e9g=', 'name@examp
 
 INSERT INTO public."bookit-users" (username, password, email, role, firstname, lastname, city)
 VALUES ('username2', 'y4t6TyLTziBF7p9CT75tfqTGmMiNMAs5dyzMZKL2e9g=', 'name2@example.com', 'HOST', 'Ime', 'Prezime', 'Novi Sad');
+
+INSERT INTO public."bookit-users" (username, password, email, role, firstname, lastname, city)
+VALUES ('username3', 'y4t6TyLTziBF7p9CT75tfqTGmMiNMAs5dyzMZKL2e9g=', 'name3@example.com', 'HOST', 'Ime2', 'Prezime2', 'Beograd');
