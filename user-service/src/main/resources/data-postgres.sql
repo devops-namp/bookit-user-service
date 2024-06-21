68d34f8c0bdb465923a14bdf689b4b6e5c71e611
pavleglusac <pavleglusac@gmail.com>
2024-06-22 01:10:11 +0200

    chore: add logs and a user in db

--- user-service/src/main/resources/data-postgres.sql
+++ user-service/src/main/resources/data-postgres.sql
@@ -9,6 +9,10 @@ VALUES (false, true, false, true, false);
 INSERT INTO public."notification-settings" (reservationrequestcreated, reservationdeclined, personalreview, accommodationreview, reservationrequestresolved)
 VALUES (true, false, true, false, true);
 
+INSERT INTO public."notification-settings" (reservationrequestcreated, reservationdeclined, personalreview, accommodationreview, reservationrequestresolved)
+VALUES (true, false, true, false, true);
+
+
 INSERT INTO public."bookit-users" (autoapprove, rejectedReservationscount, username, password, email, role, firstname, lastname, city, notification_settings_id)
 VALUES (true, 1, 'username1', 'y4t6TyLTziBF7p9CT75tfqTGmMiNMAs5dyzMZKL2e9g=', 'name@example.com', 'GUEST', 'Ime', 'Prezime', 'Novi Sad', 1);
 
@@ -17,3 +21,7 @@ VALUES (false, 2, 'username2', 'y4t6TyLTziBF7p9CT75tfqTGmMiNMAs5dyzMZKL2e9g=', '
 
 INSERT INTO public."bookit-users" (autoapprove, rejectedReservationscount, username, password, email, role, firstname, lastname, city, notification_settings_id)
 VALUES (true, 1, 'username3', 'y4t6TyLTziBF7p9CT75tfqTGmMiNMAs5dyzMZKL2e9g=', 'name3@example.com', 'HOST', 'Ime2', 'Prezime2', 'Beograd', 3);
+
+
+INSERT INTO public."bookit-users" (autoapprove, rejectedReservationscount, username, password, email, role, firstname, lastname, city, notification_settings_id)
+VALUES (true, 1, 'guestUser1', 'y4t6TyLTziBF7p9CT75tfqTGmMiNMAs5dyzMZKL2e9g=', 'guest@example.com', 'GUEST', 'Ime', 'Prezime', 'Novi Sad', 4);
